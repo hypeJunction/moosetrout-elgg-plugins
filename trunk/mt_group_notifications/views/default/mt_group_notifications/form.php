f5d8c1229644ecfcae68b6535c01b4a415b8f47c
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-05-12 19:51:56 +0000

    Clean up admin script that sets existing membership notifications.

--- trunk/mt_group_notifications/views/default/mt_group_notifications/form.php
+++ trunk/mt_group_notifications/views/default/mt_group_notifications/form.php
@@ -20,31 +20,32 @@ $form_body = "<p>" . elgg_echo('mt_group_notifications:explanation') . "</p>";
 
 // radio buttons for dryrun
 $dryrun_flag = 1;
-$form_body .= elgg_echo('mt_group_notifications:dryrun');
-$form_body .= elgg_view('input/radio', array('label' => elgg_echo('mt_group_notifications:dryrun'),
+$form_body .= "<p>" . elgg_echo('mt_group_notifications:dryrun') . "</p>";
+$form_body .= "<p>" . elgg_view('input/radio', array('label' => elgg_echo('mt_group_notifications:dryrun'),
 											'value' => $dryrun_flag,
             								'internalname' => 'dryrun',
             								'options' => array(elgg_echo('option:yes') => 1,
-                                                                elgg_echo('option:no') => 0)));
+                                                                elgg_echo('option:no') => 0))) . "</p>";
 // radio buttons for displayskipping
 $displayskipping_flag = 0;
-$form_body .= elgg_echo('mt_group_notifications:displayskipping');
-$form_body .= elgg_view('input/radio', array('label' => elgg_echo('mt_group_notifications:displayskipping'),
+$form_body .= "<p>" . elgg_echo('mt_group_notifications:displayskipping') . "</p>";
+$form_body .= "<p>" . elgg_view('input/radio', array('label' => elgg_echo('mt_group_notifications:displayskipping'),
 											'value' => $displayskipping_flag,
             								'internalname' => 'displayskipping',
             								'options' => array(elgg_echo('option:yes') => 1,
-                                                                elgg_echo('option:no') => 0)));
+                                                                elgg_echo('option:no') => 0))) . "</p>";
                                                                 
 // radio buttons for email/site/both
-$form_body .= elgg_view('input/radio', array('label' => elgg_echo('mt_group_notifications:defaultnotify'),
+$form_body .= "<p>" . elgg_echo('mt_group_notifications:setdefaults') . "</p>";
+$form_body .= "<p>" . elgg_view('input/radio', array('label' => elgg_echo('mt_group_notifications:defaultnotify'),
 											'value' => 'email',
             								'internalname' => 'defaultnotify',
             								'options' => array(elgg_echo('mt_group_notifications:optionnotifyemail') => 'email',
                                                                 elgg_echo('mt_group_notifications:optionnotifysite') => 'site',
-                                                                elgg_echo('mt_group_notifications:optionnotifyboth') => 'both')));
+                                                                elgg_echo('mt_group_notifications:optionnotifyboth') => 'both'))) . "</p>";
                                                                                                                                 
-$form_body .= elgg_view('input/submit', array('internalname' => 'submit',
-                                                'value' => elgg_echo('submit')));
+$form_body .= "<p>" . elgg_view('input/submit', array('internalname' => 'submit',
+                                                'value' => elgg_echo('submit'))) . "</p>";
 
 
 
