20bda18baab78bd7cced248c50b597262e8a9513
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-07 16:49:16 +0000

    Stub in mt_errorlog.

---
+++ trunk/mt_errorlog/views/default/settings/mt_errorlog/edit.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * mt_errorlog settings page
+ * 
+ * @author Brian Jorgensen (brianj@athabascau.ca)
+ * @copyright 2010 Brian Jorgensen
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ */
+?>
+
+<p>
+  <?php echo elgg_echo('mt_errorlog:settings:enable_email_notifications'); ?>
+ 
+  <select name="params[enable_email_notifications]">
+  <option value="true" <?php if ($vars['entity']->enable_email_notifications == 'true') echo " selected=\"yes\" "; ?>><?php echo elgg_echo('option:yes')?></option>
+  <option value="false" <?php if (($vars['entity']->enable_email_notifications == 'false') || (!isset($vars['entity']->enable_email_notifications))) echo " selected=\"yes\" "; ?>><?php echo elgg_echo('option:no')?></option>
+  </select>
+
+</p>
