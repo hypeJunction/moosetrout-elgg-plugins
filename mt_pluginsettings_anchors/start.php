88182e16a1072ce37137e8bec964b901a0df6796
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-07 22:51:14 +0000

    Add mt_pluginsettings_anchors plugin.

---
+++ mt_pluginsettings_anchors/start.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Simple Elgg Plugin to override pluginsettings view
+ * to add anchors for each plugin.
+ *
+ * @author Brian Jorgensen (brian@moosetrout.com)
+ * @copyright 2010 Brian Jorgensen
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ */
