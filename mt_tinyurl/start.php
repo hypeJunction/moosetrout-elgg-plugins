f9156911f8c25c3e659f74616c3e2fed6701c77b
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-07 22:50:04 +0000

    Stub in my_tinyurl plugin.

---
+++ mt_tinyurl/start.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * mt_tinyurl plugin
+ * Based on tinyurl plugin
+ *
+ */
+
+/**
+ * Init function
+ *
+ */
+function mt_tinyurl_init() {
+
+    // Extend useful views
+    elgg_extend_view('css','tinyurl/css');
+    	
+    // extend urlshortner view!
+    elgg_extend_view('input/urlshortener', 'my_tinyurl/addlink.php');
+    	
+    if(!is_plugin_enabled('embed')){
+        elgg_extend_view('js/initialise_elgg','mt_tinyurl/js');
+        elgg_extend_view('metatags','mt_tinyurl/metatags');
+    }
+
+    register_page_handler('tinyurl','mt_tinyurl_page_handler');
+}
+
+function mt_tinyurl_page_handler($page) {
+    switch($page[0]) {
+        default:			require_once(dirname(__FILE__) . '/longurl.php');
+        exit;
+        break;
+    }
+}
+
+/**
+ * 
+ * @param $url
+ */
+function mt_getTinyUrl($url) {
+    $tinyurl = file_get_contents("http://tinyurl.com/api-create.php?url=".$url);
+    return $tinyurl;
+}
+
+// Register the init action
+register_elgg_event_handler('init','system','mt_tinyurl_init',10);
+?>
