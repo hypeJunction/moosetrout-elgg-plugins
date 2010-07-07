eb2979763fa858aeb8b9ccd6a3e32068bed7fb69
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-07 22:50:31 +0000

    Add mt_activity_tabs plugin.

---
+++ mt_activity_tabs/languages/en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Lang file for mt_activity_tabs
+ * 
+ * @author Brian Jorgensen (brian@moosetrout.com)
+ * @copyright 2010 Brian Jorgensen
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ */
+
+$english = array(
+
+    'mt_activity_tabs' => 'Activity Tabs',
+    'mt_activity_tabs:description' => 'Enable Activity Tabs for groups and collections:',
+    'mt_activity_tabs:clickhere' => 'Click here to go to Activity Tabs.',
+    'mt_activity_tabs:empty' => 'There are no users in this group or collection.',
+    'mt_activity_tabs:addtab' => 'To add an Activity Tab for a group or collection, go to My Account -> Configure your tools -> Activity Tabs or click here.'
+);
+
+add_translation("en", $english);
---
+++ mt_activity_tabs/start.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Plugin that allows users to use pluginsettings to add collection
+ * and group tabs to their activity tool.
+ *
+ * @author Brian Jorgensen (brian@moosetrout.com)
+ * @copyright 2010 Brian Jorgensen
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ */
+
+require_once(dirname(__FILE__) . '/lib/river2.php');
+register_elgg_event_handler('init','system','mt_activity_tabs_init');
+
+/**
+ * Standard plugin init.
+ */
+function mt_activity_tabs_init() {
+
+    global $CONFIG;
+    
+    // register page handler
+    register_page_handler('activity_tabs','mt_activity_tabs_page_handler');
+    
+    // override activity menu item
+    if (isloggedin())
+    {
+        // override activity menu item
+        add_menu(elgg_echo('activity'), $CONFIG->wwwroot . "pg/activity_tabs/");
+    }
+
+}
+
+/**
+ * Page handler for activity tabs
+ *
+ * @param unknown_type $page
+ */
+function mt_activity_tabs_page_handler($page)
+{
+    global $CONFIG;
+     
+    include(dirname(__FILE__) . "/index.php");
+    return true;
+}
