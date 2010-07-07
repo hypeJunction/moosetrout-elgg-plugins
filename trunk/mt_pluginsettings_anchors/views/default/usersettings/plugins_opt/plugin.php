e5676a457235b2af306e967dc8040b5d1d0deda6
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-07 22:51:14 +0000

    Add mt_pluginsettings_anchors plugin.

---
+++ trunk/mt_pluginsettings_anchors/views/default/usersettings/plugins_opt/plugin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * New plugin usersettings view with html anchors
+ *
+ * This file renders a plugin for the admin screen, including active/deactive, manifest details & display plugin
+ * settings.
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd, Brian Jorgensen
+ * @link http://elgg.org/, http://www.moosetrout.com/elgg
+ */
+
+
+$plugin = $vars['plugin'];
+$details = $vars['details'];
+
+$active = $details['active'];
+$manifest = $details['manifest'];
+
+$user_guid = $details['user_guid'];
+if ($user_guid) $user_guid = $_SESSION['user']->guid;
+
+if (elgg_view("usersettings/{$plugin}/edit")) {
+    ?>
+<div class="contentWrapper">
+<h3 class="settings"><a name="<?php echo elgg_echo($plugin); ?>"><?php echo elgg_echo($plugin); ?></a></h3>
+
+<div id="<?php echo $plugin; ?>_settings"><?php echo elgg_view("object/plugin", array('plugin' => $plugin, 'entity' => find_plugin_usersettings($plugin, $user_guid), 'prefix' => 'user')) ?>
+</div>
+</div>
+    <?php } ?>
