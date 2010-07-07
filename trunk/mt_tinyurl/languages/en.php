42845db849c3f41326b898dc3943672560a00a28
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-07 22:50:04 +0000

    Stub in my_tinyurl plugin.

---
+++ trunk/mt_tinyurl/languages/en.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *
+ * @var unknown_type
+ */
+
+$english = array(
+
+		'shortlink:insert' => 'Insert short link',
+		'shortlink:modaltitle' => 'Short URL via TinyURL',
+		'shortlink:link' => 'URL',
+
+);
+	
+add_translation("en",$english);
---
+++ trunk/mt_tinyurl/views/default/mt_tinyurl/addlink.php
@@ -0,0 +1,3 @@
+<?php
+
+echo("<a class=\"tinyurl_option\" href=\"{$vars['url']}pg/mt_tinyurl/longurl?internalname=note\" rel=\"facebox\">" . elgg_echo('shortlink:insert') . "</a><br />");
