12837b9a6cc016e19a75f7280805187d4e1ef7f3
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-08 16:09:11 +0000

    Fix setting logic.

--- mt_rewrite_river_strings/start.php
+++ mt_rewrite_river_strings/start.php
@@ -7,10 +7,10 @@ function mt_rewrite_river_strings_init() {
 
     global $CONFIG;
 
-    if (get_plugin_setting('enabled', 'mt_rewrite_river_strings')) {
+    if (get_plugin_setting('enabled', 'mt_rewrite_river_strings') == 'true') {
         
         // register post processing hook
-        register_plugin_hook('display', 'view', 'mt_rewrite_river_strings_rewrite');
+        register_plugin_hook('display', 'view', 'mt_rewrite_river_strings');
     }
 
     // define views we want to rewrite codes on (means we don't have to process *everything*)
@@ -26,12 +26,12 @@ function mt_rewrite_river_strings_init() {
  * @param $params
  */
 
-function mt_rewrite_river_strings_rewrite($hook, $entity_type, $returnvalue, $params) {
+function mt_rewrite_river_strings($hook, $entity_type, $returnvalue, $params) {
 
     global $CONFIG;
 
     // check if enabled
-    if(!get_plugin_setting('enabled', 'mt_rewrite_river_strings')) {    
+    if(get_plugin_setting('enabled', 'mt_rewrite_river_strings') != 'true') {    
         return $returnvalue;
     }
     
