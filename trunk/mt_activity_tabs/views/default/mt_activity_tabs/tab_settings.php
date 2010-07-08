1f9e7f4fe0f1e10b95d03edca2df5c72643c280e
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-08 18:01:16 +0000

    Stub in ajax form.

---
+++ trunk/mt_activity_tabs/views/default/mt_activity_tabs/tab_settings.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * 
+ */
+
+global $CONFIG;
+
+// grab user
+$user_guid = $_SESSION['user']->guid;
+
+// grab collections for this user
+$collections = get_user_access_collections($user_guid);
+
+// grab groups this user is a member of
+$groups = get_users_membership($user_guid);
+
+// use ajax to post to /action/plugins/usersettings/save
+$url = elgg_add_action_tokens_to_url($CONFIG->wwwroot . "action/plugins/usersettings/save"); 
+
+// create form
+echo elgg_view('input/form', array('id' =>'mt_activity_tabs_settings_form', 'action' => $url));
+
+// hidden field
+echo elgg_view('input/hidden', array('name' => 'plugin', 'value' => 'mt_activity_tabs'));
+
+echo("<p>" . elgg_echo('mt_activity_tabs:description') ."<p>\n");
+
+echo("<div class=''>\n");
+echo("<h3>Collections</h3>\n");
+echo("<table border='1' cellpadding='5'>\n");
+
+$even = false;
+foreach ($collections as $collection) {
+
+    //
+    $name = $collection->name;
+    if(substr($name, 0, 7) == 'Group: ') {
+        continue;  
+    }
+    
+    $id = $collection->id;
+    $collectionid = "collection_" . $id;
+
+    if ($even) {
+        echo("<tr class='even'><td class='column_one'>$name</td>");
+    } else {
+        echo("<tr class='odd'><td class='column_one'>$name</td>");
+    }
+    echo("<td><select name='params[{$collectionid}]'>\n");
+
+    ?>
+<option value="yes"
+    <?php if ($vars['entity']->{$collectionid} == 'yes') echo " selected=\"yes\" "; ?>><?php echo elgg_echo('option:yes'); ?></option>
+<option value="no"
+    <?php if ($vars['entity']->{$collectionid} != 'yes') echo " selected=\"yes\" "; ?>><?php echo elgg_echo('option:no'); ?></option>
+</select></td></tr>
+    <?php
+    
+    // toggle even flag
+    if($even) {
+        $even = false;
+    } else {
+        $even = true;
+    }
+}
+echo("</table>\n");
+
+echo("<br /><br />\n");
+
+echo("<h3>Groups</h3>\n");
+echo("<table border='1' cellpadding='5'>\n");
+
+// even flag
+$even = false;
+foreach ($groups as $group) {
+
+    //
+    $name = $group->name;
+    
+    $id = $group->guid;
+    $groupid = "group_" . $id;
+
+    if ($even) {
+        echo("<tr class='even'><td class='column_one'>$name</td>");
+    } else {
+        echo("<tr class='odd'><td class='column_one'>$name</td>");
+    }	
+    echo("<td><select name='params[{$groupid}]'>\n");
+
+    ?>
+<option value="yes"
+    <?php if ($vars['entity']->{$groupid} == 'yes') echo " selected=\"yes\" "; ?>><?php echo elgg_echo('option:yes'); ?></option>
+<option value="no"
+    <?php if ($vars['entity']->{$groupid} != 'yes') echo " selected=\"yes\" "; ?>><?php echo elgg_echo('option:no'); ?></option>
+</select></td></tr>
+    <?php
+    
+    // toggle even flag
+    if($even) {
+        $even = false;
+    } else {
+        $even = true;
+    }
+}
+
+echo("</table>\n");
+echo("</div>\n");
+
+echo elgg_view('input/button', array(	'value' => elgg_echo('Submit'),
+										'js' => 'onclick="mt_activity_tabs_settings()"' ));
+
+echo '<div id="edt_ajax_spinner"><img src="' . $vars['url'] . '_graphics/ajax_loader.gif" /></div>';
+
+?>
+
+echo '<div id="mt_ajax_spinner"><img src="' . $vars['url'] . '_graphics/ajax_loader.gif" /></div>';
+
+<script type="text/javascript">
+function mt_activity_tabs_settings()
+{
+	$("#mt_ajax_spinner").ajaxStart(function(){
+		   $(this).show();
+		 });
+
+	$("#mt_ajax_spinner").ajaxStop(function(){
+		   $(this).hide();
+		 });
+	 
+	var inspect_type = $(".input-pulldown").val();
+	
+	$.ajax({
+		type: "POST",
+		url: "<?php echo $CONFIG->wwwroot . 'action/plugins/usersettings/save'; ?>",
+		data: $("#mt_activity_tabs_settings_form").serialize());
+		cache: false,
+		success: function(data){
+
+			// reload tabs
+		}
+	});
+}
+</script>
