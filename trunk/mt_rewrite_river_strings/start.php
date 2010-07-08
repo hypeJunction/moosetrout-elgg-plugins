043357614e184992aec462f94632709e9aee9c0e
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-08 16:03:03 +0000

    Stub in rewrite_river_strings plugin.

---
+++ trunk/mt_rewrite_river_strings/start.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ *
+ */
+function mt_rewrite_river_strings_init() {
+
+    global $CONFIG;
+
+    if (get_plugin_setting('enabled', 'mt_rewrite_river_strings')) {
+        
+        // register post processing hook
+        register_plugin_hook('display', 'view', 'mt_rewrite_river_strings_rewrite');
+    }
+
+    // define views we want to rewrite codes on (means we don't have to process *everything*)
+    $CONFIG->mt_rewrite_river_strings_views = array('object/thewire');
+
+}
+
+/**
+ *
+ * @param $hook
+ * @param $entity_type
+ * @param $returnvalue
+ * @param $params
+ */
+
+function mt_rewrite_river_strings_rewrite($hook, $entity_type, $returnvalue, $params) {
+
+    global $CONFIG;
+
+    // check if enabled
+    if(!get_plugin_setting('enabled', 'mt_rewrite_river_strings')) {    
+        return $returnvalue;
+    }
+    
+    // grab view
+    $view = $params['view'];
+
+    // grab old url and escape?
+    $oldurl = get_plugin_setting('oldurl', 'mt_rewrite_river_strings');
+
+    // escape?
+    
+    
+    // grab new url
+    $newurl = get_plugin_setting('newurl', 'mt_rewrite_river_strings');
+
+    // escape?
+    
+    
+    // do the search and replace
+    if (($view) && (in_array($view, $CONFIG->mt_rewrite_river_strings_views))) {
+
+        // Search and replace
+        $returnvalue = preg_replace($oldurl, $newurl, $view);
+        
+        // done
+        return $returnvalue;
+    }
+}
