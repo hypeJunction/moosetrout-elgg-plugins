965cecac781fe777f5139b5668a02a06f8251248
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-12 20:43:47 +0000

    Add div up arrow.

--- trunk/mt_activity_tabs/views/default/mt_activity_tabs/tab_settings.php
+++ trunk/mt_activity_tabs/views/default/mt_activity_tabs/tab_settings.php
@@ -159,6 +159,7 @@ $fb .= '<div id="mt_ajax_spinner"><img src="' . $vars['url'] . '_graphics/ajax_l
 
 echo("<div id='mt_activity_tabs_settings'>\n");
 echo("<div id='mt_activity_tabs_settings_header' />\n");
+echo("<div id='mt_activity_tabs_settings_body'>\n");
 
 // write out form
 echo elgg_view('input/form', array('body' => $fb, 'internalid' =>'mt_activity_tabs_settings_form'));
@@ -168,6 +169,7 @@ echo elgg_view('input/form', array('body' => $fb, 'internalid' =>'mt_activity_ta
 <p><img src="<?php echo $CONFIG->wwwroot ?>mod/mt_activity_tabs/_graphics/river_icon_collection.png" /><?php echo elgg_echo('mt_activity_tabs:collectionexpl')?><a href="<?php echo $CONFIG->wwwroot ?>pg/collections/add"><?php echo elgg_echo('mt_activity_tabs:createcollection')?></a></p>
 <p><img src="<?php echo $CONFIG->wwwroot ?>mod/mt_activity_tabs/_graphics/river_icon_group.png" /><?php echo elgg_echo('mt_activity_tabs:groupexpl')?><a href="<?php echo $CONFIG->wwwroot ?>pg/groups/new"><?php echo elgg_echo('mt_activity_tabs:creategroup')?></a></p>
 
+</div><!--  close mt_actitivy_tabs_settings_body div -->
 </div><!--  close mt_actitivy_tabs_settings div -->
 
 <script type="text/javascript">
