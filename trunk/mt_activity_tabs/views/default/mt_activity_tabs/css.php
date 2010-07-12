7bbbffec426faf77cbdb5dcbeb8c3a1141a46419
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-12 19:29:35 +0000

    Override river icon for calendars/events.

--- trunk/mt_activity_tabs/views/default/mt_activity_tabs/css.php
+++ trunk/mt_activity_tabs/views/default/mt_activity_tabs/css.php
@@ -7,3 +7,11 @@
 .mt_activity_tabs_wrapper {
 	margin-left: 20px;
 }
+
+// override river icons
+.river_object_event_calendar_create {
+	background: url(<?php echo $vars['url']; ?>mod/mt_activity_tabs/_graphics/river_icon_calendar.gif) no-repeat left -1px;
+}
+.river_object_event_calendar_update {
+	background: url(<?php echo $vars['url']; ?>mod/mt_activity_tabs/_graphics/river_icon_calendar.gif) no-repeat left -1px;
+}
