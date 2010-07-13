30d008986ad1873f1f4a9917478c555bc6185fa9
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-13 18:17:28 +0000

    Move js into separate file.

--- mt_activity_tabs/views/default/mt_activity_tabs/js.php
+++ mt_activity_tabs/views/default/mt_activity_tabs/js.php
@@ -2,8 +2,17 @@
 /**
  * Javascript for mt_activity_tabs
  */
-?>
 
+
+
+global $CONFIG;
+$nav_tab_endpoint_values = "orient=" . $vars['orient'] . "&type=" . $vars['type'] . "&url=" . $vars['url'] . "&user=" . $vars['user'];
+$nav_tab_endpoint_url = $CONFIG->wwwroot . 'mod/mt_activity_tabs/endpoints/nav_tabs_endpoint.php';
+
+echo($nav_tab_endpoint_values);
+echo($nav_tab_endpoint_url);
+
+?>
 <script type="text/javascript">
 $(document).ready(function () {
 
