d652a2d799dd51fda833bb462eaf7cc18b949256
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-14 20:05:22 +0000

    Add enable/disable radio for all/friends/mine.

--- mt_activity_tabs/endpoints/nav_tabs_endpoint.php
+++ mt_activity_tabs/endpoints/nav_tabs_endpoint.php
@@ -80,76 +80,94 @@ if (substr($orient, 0, 11) == 'collection_') {
 ?>
 
 <ul>
+
+<?php
+
+// clean up usersettings
+if (is_null($usersettings->all)) $usersettings->all == 'no';
+if (is_null($usersettings->friends)) $usersettings->friends == 'no';
+if (is_null($usersettings->mine)) $usersettings->mine == 'no';
+
+if ($usersettings->all == 'yes') { ?>
 	<li <?php echo $allselect; ?>><a
 		onclick="javascript:$('#river_container').load('<?php echo $url; ?>pg/activity_tabs/?display=all&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
 		href="?display="><?php echo elgg_echo('all'); ?></a></li>
+        <?php
+}
+	
+if ($usersettings->friends == 'friends') { ?>	
 	<li <?php echo $friendsselect; ?>><a
 		onclick="javascript:$('#river_container').load('<?php echo $url; ?>pg/activity_tabs/?display=friends&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
 		href="?display=friends"><?php echo elgg_echo('friends'); ?></a></li>
+        <?php
+}
+	
+if ($usersettings->all == 'mine') { ?>
 	<li <?php echo $mineselect; ?>><a
 		onclick="javascript:$('#river_container').load('<?php echo $url; ?>pg/activity_tabs/?display=mine&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
 		href="?display=mine"><?php echo elgg_echo('mine'); ?></a></li>
 		<?php
+}
 
-	// iterate through collections
-	foreach($collection_ids as $id) {
+// iterate through collections
+foreach($collection_ids as $id) {
 
-	    $collection = get_access_collection($id);
-	    $collection_name = $collection->name;
+    $collection = get_access_collection($id);
+    $collection_name = $collection->name;
 
-        // set selected class
-	    if($c_river && ($selectedid == $id)) {
-	    ?>
-	        <li class='selected'><a
-	        onclick="javascript:$('#river_container').load('<?php echo $url; ?>pg/activity_tabs/?display=collection_<?php echo $collection->id ?>&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
-	        href="?display=collection_<?php echo $collection->id; ?>"><?php echo $collection_name; ?></a></li>
-	        
-	    <?php
-	    } else {
-	    ?>
-            <li><a
-            onclick="javascript:$('#river_container').load('<?php echo $url; ?>pg/activity_tabs/?display=collection_<?php echo $collection->id ?>&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
-            href="?display=collection_<?php echo $collection->id; ?>"><?php echo $collection_name; ?></a></li>
-        <?php
-        }
-	}
+    // set selected class
+    if($c_river && ($selectedid == $id)) {
+    ?>
+        <li class='selected'><a
+        onclick="javascript:$('#river_container').load('<?php echo $url; ?>pg/activity_tabs/?display=collection_<?php echo $collection->id ?>&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
+        href="?display=collection_<?php echo $collection->id; ?>"><?php echo $collection_name; ?></a></li>
+        
+    <?php
+    } else {
+    ?>
+        <li><a
+        onclick="javascript:$('#river_container').load('<?php echo $url; ?>pg/activity_tabs/?display=collection_<?php echo $collection->id ?>&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
+        href="?display=collection_<?php echo $collection->id; ?>"><?php echo $collection_name; ?></a></li>
+    <?php
+    }
+}
 
-	// iterate through groups
-	foreach($group_ids as $id) {
+// iterate through groups
+foreach($group_ids as $id) {
 
-	    $group = get_group_entity_as_row($id);
-	    $group_name = $group->name;
+    $group = get_group_entity_as_row($id);
+    $group_name = $group->name;
 
-        // set selected class
-	    if($g_river && ($selectedid == $id)) {
-	    ?>
-	    	<li class='selected'><a
-	    	onclick="javascript:$('#river_container').load('<?php echo $url ?>pg/activity_tabs/?display=group_<?php echo $id ?>&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
-	    	href="?display=group_<?php echo $id ?>"><?php echo $group_name ?></a></li>
-	    <?php
-	    } else {
-	    ?>
-	        <li><a
-	        onclick="javascript:$('#river_container').load('<?php echo $url ?>pg/activity_tabs/?display=group_<?php echo $id ?>&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
-	        href="?display=group_<?php echo $id ?>"><?php echo $group_name ?></a></li>
-	    <?php
-	    }
-	}
+    // set selected class
+    if($g_river && ($selectedid == $id)) {
+    ?>
+    	<li class='selected'><a
+    	onclick="javascript:$('#river_container').load('<?php echo $url ?>pg/activity_tabs/?display=group_<?php echo $id ?>&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
+    	href="?display=group_<?php echo $id ?>"><?php echo $group_name ?></a></li>
+    <?php
+    } else {
+    ?>
+        <li><a
+        onclick="javascript:$('#river_container').load('<?php echo $url ?>pg/activity_tabs/?display=group_<?php echo $id ?>&amp;content=<?php echo $type; ?>,<?php echo $vars['subtype']; ?>&amp;callback=true'); return false;"
+        href="?display=group_<?php echo $id ?>"><?php echo $group_name ?></a></li>
+    <?php
+    }
+}
 	
-	// add settings tab
-	echo("<li><a href='#' id='mt_display_tab_settings'>+/-</a></li>");
-	
-	?>
-	<script type='text/javascript'>
-	$("#mt_display_tab_settings").click(function () {
-    	$('#mt_activity_tabs_settings').slideToggle("slow");
-    	return false;
-  	});
-  	</script>
-    
-	<?php
-	// add ajax spinner
-	echo("<li id='mt_ajax_spinner'><img src='" . $url . "_graphics/ajax_loader.gif' /></li>");
-	?>
+// add settings tab
+echo("<li><a href='#' id='mt_display_tab_settings'>+/-</a></li>");
+
+?>
+<script type='text/javascript'>
+$("#mt_display_tab_settings").click(function () {
+	$('#mt_activity_tabs_settings').slideToggle("slow");
+	return false;
+});
+</script>
+
+<?php
+// add ajax spinner
+echo("<li id='mt_ajax_spinner'><img src='" . $url . "_graphics/ajax_loader.gif' /></li>");
+?>
 	    
 </ul>
