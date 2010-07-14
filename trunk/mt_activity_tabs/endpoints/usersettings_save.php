2fcca96c96a5a7e826a672cebdcb5d957d41c811
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-14 18:53:51 +0000

    Stub in usersettings save ajax endpoint.

---
+++ trunk/mt_activity_tabs/endpoints/usersettings_save.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Elgg plugin user settings save action.
+ * 
+ * Modified to be an ajax endpoint.
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd, Brian Jorgensen
+ * @link http://elgg.org/, http://www.moosetrout.com
+ */
+require_once(dirname(dirname(dirname(dirname(__FILE__)))) . "/engine/start.php");
+
+gatekeeper();
+
+$params = get_input('params');
+$plugin = get_input('plugin');
+
+$result = false;
+
+foreach ($params as $k => $v) {
+	// Save
+	$result = set_plugin_usersetting($k, $v, $_SESSION['user']->guid, $plugin);
+
+	// Error?
+	if (!$result) {
+	    
+	    // return error
+		echo(sprintf(elgg_echo('plugins:usersettings:save:fail'), $plugin));
+		exit;
+	}
+}
+
+// return success
+echo(sprintf(elgg_echo('plugins:usersettings:save:ok'), $plugin));
+exit;
