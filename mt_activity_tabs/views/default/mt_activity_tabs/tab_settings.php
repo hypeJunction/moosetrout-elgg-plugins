453fb1b8be04e351e8cba006e96138fbaea2d5f7
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-14 21:08:21 +0000

    Friends tab should always be visible.

--- mt_activity_tabs/views/default/mt_activity_tabs/tab_settings.php
+++ mt_activity_tabs/views/default/mt_activity_tabs/tab_settings.php
@@ -60,20 +60,20 @@ $fb .= "<td>" . elgg_view('input/radio', array('internalid' => "default_select_a
 
 // print out row for friends
 // fix usersettings when NULL
-if (is_null($usersettings->friends)) {
-    $usersettings->friends = 'yes';
-}
-$fb .= "<tr class='even'><td class='column_one'>Friends</td>";
-$fb .= "<td>" . elgg_view('mt_activity_tabs/input/radio', array('internalid' => 'mtat_friends', 'internalname' => 'params[friends]', 'value' => $usersettings->friends, 'js' => 'onclick="mtActivityTabsToggleDefaultRadio(\'friends\', \'friends\');"', 'options' => array('yes'=>'yes', 'no'=>'no'))) . "</td>";
-
-// is default radio button disabled?
-unset($disabled);
-if ($usersettings->friends == 'no') {
-    $disabled = 'disabled';
-}
-
-// default radio button    
-$fb .= "<td>" . elgg_view('input/radio', array('internalid' => "default_select_friends", 'internalname' => 'params[default_tab]', 'disabled' => $disabled, 'value' => $usersettings->default_tab, 'options' => array(''=>'friends'))) . "</td></tr>\n";
+//if (is_null($usersettings->friends)) {
+//    $usersettings->friends = 'yes';
+//}
+//$fb .= "<tr class='even'><td class='column_one'>Friends</td>";
+//$fb .= "<td>" . elgg_view('mt_activity_tabs/input/radio', array('internalid' => 'mtat_friends', 'internalname' => 'params[friends]', 'value' => $usersettings->friends, 'js' => 'onclick="mtActivityTabsToggleDefaultRadio(\'friends\', \'friends\');"', 'options' => array('yes'=>'yes', 'no'=>'no'))) . "</td>";
+//
+//// is default radio button disabled?
+//unset($disabled);
+//if ($usersettings->friends == 'no') {
+//    $disabled = 'disabled';
+//}
+//
+//// default radio button    
+//$fb .= "<td>" . elgg_view('input/radio', array('internalid' => "default_select_friends", 'internalname' => 'params[default_tab]', 'disabled' => $disabled, 'value' => $usersettings->default_tab, 'options' => array(''=>'friends'))) . "</td></tr>\n";
 
 // print out row for mine
 // fix usersettings when NULL
