783c78f98ae3327428154af2b3b2a9b32ecb2294
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-19 18:55:27 +0000

    Stub in plugin setting for enable/disable friend.

--- trunk/mt_activity_tabs/views/default/settings/mt_activity_tabs/edit.php
+++ trunk/mt_activity_tabs/views/default/settings/mt_activity_tabs/edit.php
@@ -6,7 +6,7 @@
 <p>
   <?php echo elgg_echo('mt_activity_tabs_settings:settings:disablefriends'); ?>
  
-  <select name="params[enable_email_notifications]">
+  <select name="params[enable_disable_friends]">
   <option value="true" <?php if ($vars['entity']->enable_disable_friends == 'true') echo " selected=\"selected\" "; ?>><?php echo elgg_echo('option:yes')?></option>
   <option value="false" <?php if (($vars['entity']->enable_disable_friends == 'false') || (!isset($vars['entity']->enable_disable_friends))) echo " selected=\"selected\" "; ?>><?php echo elgg_echo('option:no')?></option>
   </select>
