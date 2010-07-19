968ff8f8665148af2a4a9d43321591dd2932c21e
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-19 19:52:38 +0000

    Fix non-javascript href.

--- trunk/mt_activity_tabs/endpoints/nav_tabs_endpoint.php
+++ trunk/mt_activity_tabs/endpoints/nav_tabs_endpoint.php
@@ -172,7 +172,7 @@ foreach($group_ids as $id) {
 }
 	
 // add settings tab
-echo("<li><a href='" . $CONFIG->wwwroot ."pg/settings/plugins#mt_activity_tabs' id='mt_display_tab_settings'>+/-</a></li>");
+echo("<li><a href='" . $CONFIG->wwwroot ."pg/settings/plugins/" . $userid  . "#mt_activity_tabs' id='mt_display_tab_settings'>+/-</a></li>");
 
 ?>
 <script type='text/javascript'>
