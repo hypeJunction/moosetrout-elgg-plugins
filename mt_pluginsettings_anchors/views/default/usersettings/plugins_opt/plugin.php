ceaf0031f7b893fd590e3a8023f7e86049c5ac06
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-19 19:33:14 +0000

    Use plugin folder name for anchor.

--- mt_pluginsettings_anchors/views/default/usersettings/plugins_opt/plugin.php
+++ mt_pluginsettings_anchors/views/default/usersettings/plugins_opt/plugin.php
@@ -24,7 +24,7 @@ if ($user_guid) $user_guid = $_SESSION['user']->guid;
 if (elgg_view("usersettings/{$plugin}/edit")) {
     ?>
 <div class="contentWrapper">
-<h3 class="settings"><a name="<?php echo elgg_echo($plugin); ?>"><?php echo elgg_echo($plugin); ?></a></h3>
+<h3 class="settings"><a name="<?php echo $plugin; ?>"><?php echo elgg_echo($plugin); ?></a></h3>
 
 <div id="<?php echo $plugin; ?>_settings"><?php echo elgg_view("object/plugin", array('plugin' => $plugin, 'entity' => find_plugin_usersettings($plugin, $user_guid), 'prefix' => 'user')) ?>
 </div>
