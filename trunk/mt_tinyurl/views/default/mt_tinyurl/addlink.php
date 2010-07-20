e6e1c85d027e0f262396250c746b951d1adf7e68
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-20 19:04:10 +0000

    Remove debug.

--- trunk/mt_tinyurl/views/default/mt_tinyurl/addlink.php
+++ trunk/mt_tinyurl/views/default/mt_tinyurl/addlink.php
@@ -6,7 +6,6 @@
 ?>
 <!-- open mt_tinyurl link -->
 <?php
-echo("whatever<br />");
 echo("<a class=\"tinyurl_option\" href=\"{$vars['url']}pg/mt_tinyurl/longurl?internalname=note\" rel=\"facebox\">" . elgg_echo('shortlink:insert') . "</a><br />");
 ?>
 <!-- close mt_tinyurl link -->
