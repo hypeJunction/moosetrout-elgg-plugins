69a712a5030240b142eebe34a0d702863b00510c
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-27 17:25:01 +0000

    Clean up tinyurl css.

--- mt_tinyurl/views/default/mt_tinyurl/addlink.php
+++ mt_tinyurl/views/default/mt_tinyurl/addlink.php
@@ -33,7 +33,7 @@ $("#mt_tinyurl_link").click(function () {
 });
 </script>
 <div id='mt_tinyurl_form'>
-<h1><?php echo elgg_echo("shortlink:modaltitle"); ?></h1>
+<h3><?php echo elgg_echo("shortlink:modaltitle"); ?></h3>
 <div id='sectionLink'><script type="text/javascript">
 function mt_tinyurl_getTinyURL(longURL, success) {
  
