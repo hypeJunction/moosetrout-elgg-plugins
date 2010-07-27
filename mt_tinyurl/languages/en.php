1121662c1bdb6d8dfcbc008b8369813338021032
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-27 17:35:23 +0000

    Clean up tinyurl css.

--- mt_tinyurl/languages/en.php
+++ mt_tinyurl/languages/en.php
@@ -8,7 +8,7 @@
 $english = array(
 
 		'shortlink:insert' => 'Insert short link',
-		'shortlink:modaltitle' => 'Short URL via TinyURL',
+		'shortlink:modaltitle' => 'Insert Short URL via TinyURL',
 		'shortlink:link' => 'URL',
     	'mt_tinyurl:settings:facebox' => 'facebox',
         'mt_tinyurl:settings:accordion' => 'accordion'
