04d25d20a1c9b3d11aa648be17a3bd20f0e302cc
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-28 01:36:11 +0000

    Change +/- to close link.

--- trunk/mt_activity_tabs/endpoints/nav_tabs_endpoint.php
+++ trunk/mt_activity_tabs/endpoints/nav_tabs_endpoint.php
@@ -187,13 +187,13 @@ $("#mt_display_tab_settings").click(function () {
 	
 	if($('#mt_activity_tabs_usersettings').is(':visible')) {
 		alert('closing!');
-		$('#mt_display_tab_settings').text('\(<?php elgg_echo('mt_activity_tabs:close') ?>\)');
+		$('#mt_display_tab_settings').text('\(<?php echo elgg_echo('mt_activity_tabs:close') ?>\)');
 	}
 
 	// making visible
 	if($('#mt_activity_tabs_usersettings').is(':hidden')) {
 		alert('opening!');
-		$('#mt_display_tab_settings').text('\(<?php elgg_echo('mt_activity_tabs:open') ?>\)');
+		$('#mt_display_tab_settings').text('\(<?php echo elgg_echo('mt_activity_tabs:open') ?>\)');
 	}
 		
 	return false;
