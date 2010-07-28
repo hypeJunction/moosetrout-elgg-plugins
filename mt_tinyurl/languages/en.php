445997ff968b0044f1eb61fe62f1d1945de929aa
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-28 02:15:58 +0000

    Fix lang strings.

--- mt_tinyurl/languages/en.php
+++ mt_tinyurl/languages/en.php
@@ -8,7 +8,7 @@
 $english = array(
 
 		'mt_tinyurl:insert' => 'Insert short link',
-        'mt_tinyurl:close' => '(close)',
+        'mt_tinyurl:close' => 'close',
 		'mt_tinyurl:modaltitle' => 'Insert Short URL via TinyURL',
 		'mt_tinyurl:link' => 'URL',
     	'mt_tinyurl:settings:facebox' => 'facebox',
