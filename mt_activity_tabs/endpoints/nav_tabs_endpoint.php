a78ce7794c0b79a2f6b5f6de42d2bd6fbee8353a
moosetrout <moosetrout@78201c23-33cf-29f5-cf14-cd9f60b80fed>
2010-07-28 01:39:55 +0000

    Add debug.

--- mt_activity_tabs/endpoints/nav_tabs_endpoint.php
+++ mt_activity_tabs/endpoints/nav_tabs_endpoint.php
@@ -195,6 +195,10 @@ $("#mt_display_tab_settings").click(function () {
 		alert('opening!');
 		$('#mt_display_tab_settings').text('\(<?php echo elgg_echo('mt_activity_tabs:open') ?>\)');
 	}
+
+	if($('#mt_activity_tabs_usersettings').text() == '(close)' {
+		alert('OPENING!');
+	}
 		
 	return false;
 });
